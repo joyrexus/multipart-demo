c4078a7b7567c7c0e0686311838ce5ab9ce8b537
souparno majumder <souparno.majumder@gmail.com>
2017-01-16 22:02:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,10 +15,35 @@ server.route({
 server.route({ 
     method: 'POST', 
     path: '/submit', 
-    config: { 
-        payload: { output: 'stream' },
+    config: {
+        payload: {
+            output: 'stream',
+            parse: true,
+            allow: 'multipart/form-data'
+        },
+
         handler: function (request, reply) {
-            reply(request.payload);
+            var data = request.payload;
+            if (data.file) {
+                var name = data.file.hapi.filename;
+                var path = __dirname + "/uploads/" + name;
+                var file = fs.createWriteStream(path);
+
+                file.on('error', function (err) { 
+                    console.error(err) 
+                });
+
+                data.file.pipe(file);
+
+                data.file.on('end', function (err) { 
+                    var ret = {
+                        filename: data.file.hapi.filename,
+                        headers: data.file.hapi.headers
+                    }
+                    reply(JSON.stringify(ret));
+                })
+            }
+
         }
     } 
 });
